6cc3b145e8412e9e03e45115fc712060c735a939
nogueirajoao <joao_jvn@outlook.com>
2019-08-01 18:08:09 +0100

    Stripe Api

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,19 +1,45 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import { Link } from 'react-router-dom';
+
+import StripeBilling from './StripeBilling';
 
 class Header extends Component {
+
+    renderContent() {
+        switch (this.props.auth) {
+            case null:
+                return;
+            case false:
+                return (
+                    <li>
+                        <a href="/auth/google">Login with Google</a>
+                    </li>
+                );
+            default:
+                return [
+                    <li key="1">
+                        <StripeBilling />
+                    </li>,
+                    <li key ="2" style={ { margin: '0 10px'}}>
+                        Credits: {this.props.auth.credits}
+                    </li>,
+                    <li key="3">
+                        <a href="/api/logout">Logout</a>
+                    </li>
+                ];                                  
+        }
+    }
+
     render() {
-        console.log(this.props);
         return(
             <nav>
                 <div className="nav-wrapper">
-                    <a className="brand-logo">
+                    <Link to={this.props.auth ? '/surveys' : '/'} className="brand-logo">
                         Emaily
-                    </a>
+                    </Link>
                     <ul className="right hide-on-med-and-down">
-                        <li>
-                            <a>Login With Google</a>
-                        </li>
+                        {this.renderContent()}
                     </ul>
                 </div>
             </nav>
---
+++ client/src/components/Landing.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+class Landing extends React.Component {
+    render() {
+        return (
+            <div style={{ textAlign: 'center' }}>
+                <h1>
+                    Emaily!
+                </h1>
+                Collect feedback from the users
+            </div>
+        );
+    }
+};
+
+export default Landing;
